165c6825a965bda53cf4b75dfe6a905c8f123e24
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2022-06-19 23:45:07 -0400

    ~/.quicket and configuration
    and QUICKET_HOME and QUICKET_CONF env variables

--- lib/Quicket.py
+++ lib/Quicket.py
@@ -1,11 +1,42 @@
 
+from os import environ as env
 from pathlib import Path
+import pwd
+import toml
 
 class Quicket():
     def __init__(self, config_dir=None):
         if config_dir is None:
-            config_dir = "/home/mjd/.quicket" # XXX should default to $HOME/.quicket
+            config_dir = self.default_config_dir()
         self.config_dir = Path(config_dir)
+
+        self.configure()
+
+    def default_config_dir(self):
+        if "QUICKET_HOME" in env:
+            return Path(env["QUICKET_HOME"])
+        else:
+            return self.homedir() / ".quicket"
+
+    def default_config_file(self):
+        if "QUICKET_CONF" in env:
+            return Path(env["QUICKET_CONF"])
+        else:
+            return self.default_config_dir() / "config"
+
+    def homedir(self):
+        if "HOME" in env:
+            return Path(env["HOME"])
+        else:
+            uid = os.getuid()
+            return Path(pwd.getpwuid(uid).pw_dir)
+
+    def configure(self):
+        try:
+            with open(self.default_config_file()) as fh:
+                self.conf = toml.load(fh)
+        except FileNotFoundError:
+            self.conf = {}
 
     @property
     def ticket_dir(self):
